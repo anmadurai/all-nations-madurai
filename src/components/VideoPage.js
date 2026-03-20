538fc8eadc422625d76cda3ffb6df74fc46b7597
anmadurai <anmadurai712@gmail.com>
2026-03-20 21:52:53 +0530

    Update VideoPage.js

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import '../styles/VideoPage.css'; 
+import '../styles/VideoPage.css'; KyJelB0bmxM
 
 const VideoPage = () => {   
   const videos = [
-    { id: '81dz3-1nuQ8 ', title: 'Sunday Evening Service 15-03-26', img: 'sm-01-02-26.png' },
+    { id: 'KyJelB0bmxM', title: 'Friday All Night Service 20-03-26', img: 'fri-19-12-25.png' },
+    { id: '81dz3-1nuQ8', title: 'Sunday Evening Service 15-03-26', img: 'sm-01-02-26.png' },
     { id: 'uhGf9Zhc_Ig', title: 'Sunday Evening Service 08-03-26', img: 'sm-01-02-26.png' },
     { id: 'VkN1oPyn6fk', title: 'Sunday Morning Service 08-03-26', img: 'sm-01-02-26.png' },
     { id: '7d2Ow5FItso', title: 'Sunday Evening Service 01-03-26', img: 'sm-01-02-26.png' },
